633c7a99ee1a3d456501b190d6523dc57f6ee445
Aizirek T <aizirekt98@gmail.com>
2020-04-07 13:58:35 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,12 @@
-node { 
+node ( 
+    properties([ 
+       // Bellow line sets "Discard Builds more than 5"
+       properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('* * *  *  *')])])
+       // Bellow line triggers this job every minute
+       pipelineTriggers{[cron('* * * * *')]} 
+      ]}
+
+
    stage("Stage1"){
        echo "hello" 
 } 
