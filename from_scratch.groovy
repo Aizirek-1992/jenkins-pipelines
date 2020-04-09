2fa4c9dce59e6e7552bf56d39e10857447301f97
Aizirek-1992 <59779005+Aizirek-1992@users.noreply.github.com>
2020-04-09 14:39:42 -0500

    Update from_scratch.groovy

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -5,19 +5,27 @@ properties([
     // Bellow line triggers this job every minute
 	pipelineTriggers([cron('* * * * *')])])
 
-    stage("Stage1"){
-		echo "hello"
+    stage("Pull Repo"){
+		git 'https://github.com/farrukh90/cool_website.git'
 }
-	stage("Stage2"){
-		echo "hello"
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.aizirek.org                sudo yum install httpd -y
+		"""
 }
-	stage("Stage3"){
-		echo "hello"
+	stage("Copy artifacts"){
+		sh """
+		scp -r * centos@jenkins_worker1.aizirek.org:/tmp
+		ssh centos@jenkins_worker1.aizirek.org                 sudo cp -r /tmp/index.html /var/www/html
+		ssh centos@jenkins_worker1.aizirek.org                 sudo cp -r /tmp/style.css /var/www/html
+		ssh centos@jenkins_worker1.aizirek.org                 sudo chown centos:centos /var/www/html
+		ssh centos@jenkins_worker1.aizirek.org                 sudo chmod 777 /var/www/html/*
+		"""
 }
-	stage("Stage4"){
-		echo "hello"
+	stage("Restart web server"){
+		sh "ssh centos@jenkins_worker1.aizirek.org             sudo systemctl restart httpd"
 }
-	stage("Stage5"){
-		echo "hello"
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello,World!'
 	}
 }
