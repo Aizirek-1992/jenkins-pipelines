44d1c7c862587cd0769d3662e0759ba88c881f33
Aizirek T <aizirekt98@gmail.com>
2020-04-09 14:11:28 -0500

    n

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,5 +1,6 @@
 node {
-properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('* * * * *')])])
+properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+ pipelineTriggers([cron('* * * * *')])])
 
     stage("Stage1"){
 		echo "hello"
