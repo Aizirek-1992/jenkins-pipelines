c530e840c9085d47d69e81419aaa66facc009446
Aizirek T <aizirekt98@gmail.com>
2020-04-08 20:08:14 -0500

    added proporties

--- Template.groovy
+++ Template.groovy
@@ -1,5 +1,5 @@
 node { 
-<<<<<<< HEAD
+
    stage("Stage1"){
        echo "hello" 
 } 
@@ -16,21 +16,5 @@ node {
        echo "hello" 
    } 
 } 
-=======
-stage("Stage1"){ 
-echo "hello" 
-} 
-stage("Stage2"){ 
-echo "hello" 
-} 
-stage("Stage3"){ 
-echo "hello" 
-} 
-stage("Stage4"){ 
-echo "hello" 
-} 
-stage("Stage5"){ 
-echo "hello"
-} 
-} 
->>>>>>> 0a9bf7e39bed16fe3ed63c4ad25848d98fe41d04
+
+
