23df2d54f2ca5f692290d69cb972fac1b65d2b84
Aizirek-1992 <59779005+Aizirek-1992@users.noreply.github.com>
2020-04-09 14:16:00 -0500

    Update from_scratch.groovy

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,17 +1,23 @@
-node {  
-     stage("Stage1"){
-       echo "hello" 
-} 
-   stage("Stage2"){ 
-       echo "hello" 
-} 
-   stage("Stage3"){ 
-       echo "hello" 
-} 
-   stage("Stage4"){ 
-       echo "hello" 
-} 
-   stage("Stage5"){ 
-       echo "hello" 
-   } 
-} 
+node {
+properties([
+	// Bellow line sets "Discard Builds more than 5"
+	buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+    // Bellow line triggers this job every minute
+	pipelineTriggers([cron('* * * * *')])])
+
+    stage("Stage1"){
+		echo "hello"
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		echo "hello"
+	}
+}
